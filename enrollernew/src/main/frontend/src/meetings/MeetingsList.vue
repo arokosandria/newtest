088a0d8b0b12226977db62d75c34a3eb89c6ecce
arokosandria <kmsa@interia.pl>
2019-05-19 13:50:12 +0200

    edjt

--- enrollernew/src/main/frontend/src/meetings/MeetingsList.vue
+++ enrollernew/src/main/frontend/src/meetings/MeetingsList.vue
@@ -11,7 +11,7 @@
     <tbody>
     <tr v-for="meeting in meetings" :key="meeting.id">
       <td>{{ meeting.title }}</td>
-      <td>{{ meeting.description }}</td>
+  <td>{{ meeting.description }}</td>
       <td>
         <ul v-if="meeting.participants">
           <li v-for="participant in meeting.participants" :key="participant">
