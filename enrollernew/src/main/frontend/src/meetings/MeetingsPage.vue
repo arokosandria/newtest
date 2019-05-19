cc837f0af93c0d82c442408bad6ff650f9ddb97e
arokosandria <kmsa@interia.pl>
2019-05-19 13:00:51 +0200

    xxx

---
+++ enrollernew/src/main/frontend/src/meetings/MeetingsPage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <new-meeting-form @added="addNewMeeting($event)"></new-meeting-form>
+
+    <span v-if="meetings.length == 0">
+               Brak zaplanowanych spotkań.
+           </span>
+    <h3 v-else>
+      Zaplanowane zajęcia ({{ meetings.length }})
+    </h3>
+
+    <meetings-list :meetings="meetings"
+                   :username="username"
+                   @attend="addMeetingParticipant($event)"
+                   @unattend="removeMeetingParticipant($event)"
+                   @delete="deleteMeeting($event)"></meetings-list>
+  </div>
+</template>
+
+<script>
+    import NewMeetingForm from "./NewMeetingForm";
+    import MeetingsList from "./MeetingsList";
+    export default {
+        components: {NewMeetingForm, MeetingsList},
+        props: ['username'],
+        data() {
+            return {
+                meetings: [],
+                meetingslist: []
+            };
+        },
+     mounted(){
+            this.getMeetings();
+           
+        
+        },
+        
+        methods: {
+        	getMeetings(){
+        	this.$http.get('meetings')
+                     .then(response => {
+                         this.meetings = response.body;
+                         
+                     });
+        	
+        	},
+        	
+           addNewMeeting(meeting) {
+                this.$http.post('meetings', meeting)
+                .then(response => this.meetings.push(response.body));
+                    
+            },
+           
+            deleteMeeting(meeting) {
+                this.$http.delete('meetings/'+ meeting.id);
+                this.meetings.splice(this.meetings.indexOf(meeting), 1);
+            },
+            }
+        }
+    
+</script>
